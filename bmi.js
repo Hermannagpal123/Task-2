aef24306a140003a9c28ec0ac294f4cc5d7bf470
madhavkeshavsingla <madhav1366.be23@chitkara.edu.in>
2024-04-09 19:46:21 +0530

    bmi.js update

---
+++ bmi.js
@@ -0,0 +1,10 @@
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const bmi_value = document.querySelector('#bmi-value');
+const bmi_status = document.querySelector('#bmi-status');
+const calculate = document.querySelector('#calculate-bmi');
+var dietPlanLink = document.getElementById('dietPlanLink');
+
+calculate.addEventListener('click', () => {
+   if( height.value === "" || weight.value === "" ) {
+        alert('Please enter your height and weight');
